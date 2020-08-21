1c541043e373655571230420e711d2be65257df8
Lucas Borges <45474687+lucasborgesdev@users.noreply.github.com>
2020-08-20 22:22:31 -0300

    ajustado filename

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ var fs = require("fs");
 // Cria o objeto server
 var app = http.createServer(callback);
 
-app.listen(process.env.PORT || 3000); // Especifica a porta que vai escutar as requisições
+app.listen(process.env.APP_URL || 3000); // Especifica a porta que vai escutar as requisições
 
 function callback(req, res) {
   // Nome do arquivo
-  var filename = (req.url = "/" ? "index.html" : __dirname + req.url);
+  var filename = req.url == "/" ? "index.html" : __dirname + req.url;
 
   fs.readFile(filename, function (err, data) {
     if (err) {
